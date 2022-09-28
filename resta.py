f27b1293686eba3e04824367b234c02dc2aa4ad0
Ronald Eusebio <roneusegit@gmail.com>
2022-09-28 11:07:57 -0400

    modificaciones en el ambito de practica

--- resta.py
+++ resta.py
@@ -1,8 +1,4 @@
 a = int(input("ingresa tu numero: "))
 b = int(input("ingresa tu numero: "))
 c = a - b
-<<<<<<< HEAD
 print("el proceso de la resta es:", c)
-=======
-print("el proceso de la resta es:", c)
->>>>>>> ramab
--- suma.py
+++ suma.py
@@ -1,8 +1,4 @@
 a = int(input("ingresa tu numero: "))
 b = int(input("ingresa tu numero: "))
 c = a + b
-<<<<<<< HEAD
 print("el proceso de la suma es:", c)
-=======
-print("el proceso de la suma es:", c)
->>>>>>> ramab
